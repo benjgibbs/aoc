a4075858e16154837781a1c4322c66211315a128
Ben Gibbs <benjgibbs@gmail.com>
2021-12-01 11:26:31 +0000

    Day 1 tidied up

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,58 +1,34 @@
-
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use aoc::read_lines;
 
 fn main() {
-
     if let Ok(lines) = read_lines("./input/day1.txt") {
+        let depths: Vec<i32> = lines
+            .filter(|br| br.is_ok())
+            .map(|br| br.unwrap().parse::<i32>().unwrap())
+            .collect();
+
         let mut last = 0;
         let mut count = 0;
         let mut first = true;
-        let mut depths : Vec<i32> = Vec::new();
 
-        for line in lines {
-            if let Ok(ip) = line {
-                let current = ip.parse::<i32>().unwrap(); 
-                depths.push(current);
-                if first {
-                    first = false;
-                } else {
-                    if current > last {
-                        count += 1;
-                    }
+        for current in depths.iter() {
+            if first {
+                first = false;
+            } else {
+                if *current > last {
+                    count += 1;
                 }
-                last = current;
             }
-    
+            last = *current;
         }
-        println!("Depth increased: {}", count);
+        println!("Part 1: {}", count);
 
-        
-        let mut sums : Vec<i32> = Vec::new();
         count = 0;
-
-        for i in 2..depths.len() {
-            sums.push(depths[i] + depths[i-1] + depths[i-2]);
-        }
-        last = sums[0];
-        for i in 1..sums.len() {
-            if sums[i] > last {
+        for i in 0..depths.len() - 3 {
+            if depths[i] < depths[i + 3] {
                 count += 1;
             }
-            last = sums[i];
         }
-
-        println!("Depth increased: {}", count);
-
+        println!("Part 2: {}", count);
     }
-
 }
