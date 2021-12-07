fb6a0939628af2ebcf9ed32d08d6db670434fcf7
Ben Gibbs <benjgibbs@gmail.com>
2021-12-07 09:54:10 +0000

    day 7 tidied up

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,43 +1,33 @@
-
 use aoc::{get_nums, read_lines};
 
 fn main() {
     if let Ok(lines) = read_lines("./input/day7.txt") {
         let line = lines.get(0).unwrap();
         let nums = get_nums(line);
-        let mut min_cost = i32::MAX;
-        let mut min_pos = 0;
-        let min = *nums.iter().min().unwrap();
-        let max = *nums.iter().max().unwrap();
-        for p in min..=max {
-            let mut cost = 0;
-            for i in nums.iter() {
-                cost += (p - i).abs();
-            }
-            if cost < min_cost {
-                min_pos = p;
-                min_cost = cost;
-            }
-        }
-        println!("Part1 (341558): {}, {}", min_pos, min_cost);
-        
-        let mut min_cost = i32::MAX;
-        let mut min_pos = 0;
-        for p in min..=max {
-            let mut cost = 0;
-            for i in nums.iter() {
-                cost += calc_cost((p - i).abs());
-            }
-            if cost < min_cost {
-                min_pos = p;
-                min_cost = cost;
-                
-            }
-        }
-        println!("Part2 (93214037): {}, {}", min_pos, min_cost);
+
+        let part1 = calc(&nums, |x|x);
+        println!("Part1 (341558): {}",  part1);
+
+        let part2 = calc(&nums, |x|x*(x+1)/2);
+        println!("Part2 (93214037): {}",  part2);
     }
 }
 
-fn calc_cost(m : i32) -> i32 {
-    return m*(m+1)/2;
+fn calc<P>(crabs: &Vec<i32>, cost_func: P) -> i32
+where
+    P: Fn(i32) -> i32,
+{
+    let mut min_cost = i32::MAX;
+    let min = *crabs.iter().min().unwrap();
+    let max = *crabs.iter().max().unwrap();
+    for p in min..=max {
+        let mut cost = 0;
+        for i in crabs.iter() {
+            cost += cost_func((p - i).abs());
+        }
+        if cost < min_cost {
+            min_cost = cost;
+        }
+    }
+    return min_cost;
 }
