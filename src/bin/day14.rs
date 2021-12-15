3b3ad9cecf91a96205a599873c79b7a0b7c5f481
Ben Gibbs <benjgibbs@gmail.com>
2021-12-15 09:00:44 +0000

    day15 done

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,77 +1,43 @@
-use std::{collections::HashMap, char};
+use std::{collections::{HashMap, BTreeMap}, char};
 
 use aoc::read_lines;
 
-fn read_input(lines: &Vec<String>) -> (Vec<char>, HashMap<String,char>) {
+fn read_input(lines: &Vec<String>) -> (Vec<char>, HashMap<(char, char),Vec<(char, char)>>) {
     let template = lines[0].to_string();
-    let mut rule_map : HashMap<String, char> = HashMap::new();
+    let mut rule_map : HashMap<(char,char), Vec<(char,char)>> = HashMap::new();
     for rule in lines.iter().skip(2) {
         let rule_parts :Vec<&str> = rule.split(" -> ").collect();
-        rule_map.insert(rule_parts[0].to_string(), rule_parts[1].as_bytes()[0] as char);
+        let pair = rule_parts[0].to_string();
+        let p1 = pair.as_bytes()[0] as char;
+        let p2  = pair.as_bytes()[1] as char;
+        let m = rule_parts[1].as_bytes()[0] as char;
+        rule_map.insert((p1,p2), Vec::from([(p1,m), (m,p2)]) );
     }
-
     let buffer : Vec<char> = template.chars().collect();
     (buffer, rule_map)
 }
 
-fn next_step(buffer: &Vec<char>, rule_map: &HashMap<String,char>) -> Vec<char> {
-    let mut next: Vec<char> = Vec::new();
-    next.push(buffer[0]);
-    for i in 0..buffer.len() - 1{
-        let check : String = [buffer[i], buffer[i+1]].iter().collect();
-        if let Some(c) = rule_map.get(&check) {
-            next.push(*c);
-        }
-        next.push(buffer[i+1]);
-    }
-    next
-}
-
-fn get_counts(buffer: &Vec<char>) -> Vec<(char, u64)> {
-    let mut counts : HashMap<char, u64> = HashMap::new();
-    for c in buffer {
-        *counts.entry(*c).or_insert(0u64) += 1;
-    }
-    let mut minmax : Vec<(char, u64)> = counts.into_iter().collect();
-    minmax.sort_by(|a,b| b.1.cmp(&a.1));
-    minmax
-}
-
 fn part1(lines: &Vec<String>) -> u64 {
-    let (mut buffer, rule_map) = read_input(lines);
-    
-    for _i in 0..10 {
-        buffer = next_step(&buffer, &rule_map);
-    }
-
-    let minmax = get_counts(&buffer);
-
-    return minmax[0].1 - minmax[minmax.len()-1].1;
+    run(lines, 10)
 }
-
 
 fn part2(lines: &Vec<String>) -> u64 {
+    run(lines, 40)
+}
+
+fn run(lines: &Vec<String>, iters: u16) -> u64 {
     let (buffer, rule_map) = read_input(lines);
-
-    let rule_map2: HashMap<(char,char), Vec<(char,char)>> = 
-        rule_map.iter().map(|kvp| {
-            let key = kvp.0.as_bytes();
-            let k1 = key[0] as char;
-            let k2 = key[1] as char;
-            let m = *kvp.1;
-            ((k1,k2), Vec::from([(k1,m), (m, k2)]))
-            }).collect();
-
-    let mut pair_count: HashMap<(char, char), u64> = HashMap::new();
+    
+    let mut pair_count: BTreeMap<(char, char), u64> = BTreeMap::new();
     for i in 0..buffer.len() -1 {
         let p = (buffer[i], buffer[i+1]);
         *pair_count.entry(p).or_insert(0) += 1;
     }
 
-    for _i in 0..40 {
-        let mut next_pair_count: HashMap<(char, char), u64> = HashMap::new();
+    for _i in 0..iters {
+        let mut next_pair_count: BTreeMap<(char, char), u64> = BTreeMap::new();
         for p in pair_count {
-            for q in rule_map2[&p.0].iter() {
+            for q in rule_map[&p.0].iter() {
                 *next_pair_count.entry(*q).or_insert(0) += p.1;
             }
         }
@@ -82,6 +48,7 @@ fn part2(lines: &Vec<String>) -> u64 {
         *sums.entry(p.0.0).or_insert(0) += p.1;
         *sums.entry(p.0.1).or_insert(0) += p.1;
     }
+    
     let mut minmax : Vec<(char, u64)> = sums.into_iter().collect();
     minmax.sort_by(|a,b| b.1.cmp(&a.1));
 
@@ -91,7 +58,6 @@ fn part2(lines: &Vec<String>) -> u64 {
 fn main() {
     if let Ok(lines) = read_lines("./input/day14.txt") {
         println!("Part 1: {}", part1(&lines));
-        //6919645078901
         println!("Part 2: {}", part2(&lines));
     }
 }
---
+++ src/bin/day15.rs
@@ -0,0 +1,164 @@
+use std::{cmp::{max, Ordering, min}, collections::BinaryHeap};
+
+use aoc::read_lines;
+
+
+struct Cave {
+    risk: Vec<u32>,
+    max_x: usize,
+    max_y: usize,
+}
+
+impl Cave {
+    pub fn new(lines: &Vec<String>) -> Cave {
+        let mut risk : Vec<u32> = Vec::new();
+        let mut max_x : usize = 0;
+
+        for line in lines.iter() {
+            max_x = max(max_x, line.len());
+            risk.append(&mut line.chars().map(|c| c.to_digit(10).unwrap() ).collect());
+        }
+        Cave { risk: risk, max_x: max_x, max_y: lines.len() }
+    }
+
+    pub fn risk(&self, i: usize) -> u32 {
+        self.risk[i]
+    }
+
+    pub fn risk_by_pos(&self, p: (usize,usize)) -> u32 {
+        self.risk[self.pos_to_idx(p)]
+    }
+
+    pub fn neighbours(&self, i: usize) -> Vec<usize> {
+        let adjacents: Vec<(i32, i32)> = vec![(-1, 0), (1, 0), (0, -1), (0, 1)];
+        let pos = self.idx_to_pos(i);
+        adjacents.iter().map(|a| (pos.0 as i32 + a.0, pos.1 as i32 + a.1))
+            .filter(|p|p.0 >= 0 && p.1 >= 0 && (p.0 as usize) < self.max_x && (p.1 as usize) < self.max_y)
+            .map(|p| self.pos_to_idx((p.0 as usize, p.1 as usize)))
+            .collect()
+    }
+
+    fn idx_to_pos(&self, i: usize) -> (usize, usize) {
+        (i % self.max_x, i / self.max_x)
+    }
+
+    fn pos_to_idx(&self, pos: (usize, usize)) -> usize {
+        pos.0 + pos.1 * self.max_x
+    }
+
+    fn extend(&self, by: usize) -> Cave {
+        let mut risks = Vec::new();
+        
+        for y in 0..self.max_y*by {
+            for x in 0..self.max_x*by {
+                let mut risk = 0;
+                if x < self.max_x && y < self.max_x {
+                    risk = self.risk_by_pos((x, y));
+                } else if y < self.max_y { // x >= max
+                    let xpos= x - self.max_x;
+                    let ypos =y;
+                    risk = risks[xpos + ypos * self.max_x * by] + 1;
+                } else  {
+                    let xpos= x;
+                    let ypos =y - self.max_y;
+                    risk = risks[xpos + ypos * self.max_x * by] + 1;
+                }
+
+                risks.push(max(risk % 10, 1));
+            }
+        }
+
+        Cave{risk: risks, max_x: self.max_x * by, max_y: self.max_y *by }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    risk: u32,
+    pos: usize
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.risk.cmp(&self.risk)
+            .then_with(|| self.pos.cmp(&other.pos))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn search(cave: Cave) -> Option<u32> {
+    let end = cave.pos_to_idx((cave.max_x-1, cave.max_y-1));
+    let mut heap = BinaryHeap::new();
+    heap.push(State{ risk: 0, pos: 0});
+
+    let mut min_risks : Vec<_> = (0..cave.max_x*cave.max_y).map(|_| u32::MAX).collect(); 
+
+    while let Some(state) = heap.pop() {
+        if state.pos == end { return Some(state.risk) }
+
+        if state.risk > min_risks[state.pos] {
+            continue;
+        }
+
+        for n in cave.neighbours(state.pos) {
+            let next = State{risk: state.risk + cave.risk(n), pos: n};
+            if next.risk < min_risks[next.pos] {
+                heap.push(next);
+                min_risks[next.pos] = next.risk;
+            }
+        }
+
+    }
+    None
+}
+
+
+fn part1(lines: &Vec<String>) -> Option<u32> {
+    let cave = Cave::new(lines);
+    search(cave)
+}
+
+fn part2(lines: &Vec<String>) -> Option<u32> {
+    let mut cave = Cave::new(lines);
+    cave = cave.extend(5);
+
+    for y in 0..cave.max_y {
+        for x in 0..cave.max_x {
+            print!("{}", cave.risk_by_pos((x,y)));
+        }
+        println!();
+    }
+
+    search(cave)
+}
+
+fn main() {
+    if let Ok(lines) = read_lines("./input/day15.txt") {
+        println!("Part 1: {:?}", part1(&lines));
+        println!("Part 2: {:?}", part2(&lines));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day15_example1() {
+        if let Ok(lines) = read_lines("./input/example15.txt") {
+            assert_eq!(Some(40), part1(&lines));
+        }
+    }
+
+    #[test]
+    fn day15_example2() {
+        if let Ok(lines) = read_lines("./input/example15.txt") {
+            assert_eq!(Some(315), part2(&lines));
+        }
+    }
+}
