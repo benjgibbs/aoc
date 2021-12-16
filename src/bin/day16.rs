d9ec117a62233beb622bafc18167ce85c3b63749
Ben Gibbs <benjgibbs@gmail.com>
2021-12-16 17:37:51 +0000

    Some tidying

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,15 +1,14 @@
 use aoc::read_lines;
-use bitvec::{prelude::*, order::Msb0};
+use bitvec::{order::Msb0, prelude::*};
 
-
-const SUM : u64 = 0;
-const PRODUCT : u64 = 1;
-const MIN : u64 = 2;
-const MAX : u64 = 3;
-const LITERAL : u64 = 4;
-const GREATER : u64 = 5;
-const LESS : u64 = 6;
-const EQ : u64 = 7;
+const SUM: u64 = 0;
+const PRODUCT: u64 = 1;
+const MIN: u64 = 2;
+const MAX: u64 = 3;
+const LITERAL: u64 = 4;
+const GREATER: u64 = 5;
+const LESS: u64 = 6;
+const EQ: u64 = 7;
 
 fn decode(input: &str) -> BitVec<Msb0, u8> {
     let mut memory = BitVec::<Msb0, u8>::new();
@@ -27,24 +26,15 @@ fn decode(input: &str) -> BitVec<Msb0, u8> {
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 enum LengthType {
-    Bits(u64,usize), //bits len, offset
-    Packets(u64,usize) //num packets, offset
+    Bits(u64),
+    Packets(u64),
 }
 
-impl LengthType {
-    fn new_bits(bits: u64) -> LengthType {
-        LengthType::Bits(bits, 16)
-    }
-    fn new_packets(packets: u64 )-> LengthType {
-        LengthType::Packets(packets, 12)
-    }
-}
-
-fn length(memory: &BitSlice<Msb0, u8>) -> LengthType {
+fn length(memory: &BitSlice<Msb0, u8>) -> (usize, LengthType) {
     if memory[6] == false {
-        LengthType::new_bits(to_u64(&memory[7..22]))
+        (16, LengthType::Bits(to_u64(&memory[7..22])))
     } else {
-        LengthType::new_packets(to_u64(&memory[7..18]))
+        (12, LengthType::Packets(to_u64(&memory[7..18])))
     }
 }
 
@@ -62,65 +52,15 @@ fn read_literal(memory: &BitSlice<Msb0, u8>) -> (usize, u64) {
     let mut result = BitVec::<Msb0, u8>::new();
     let mut i = 6;
     let mut has_another_packet = true;
-    while  has_another_packet {
+    while has_another_packet {
         has_another_packet = memory[i];
         for j in 0..4 {
-            result.push(memory[i+1 +j]);
+            result.push(memory[i + 1 + j]);
         }
-        i += 5;        
+        i += 5;
     }
     (i, to_u64(&result))
 }
-
-fn version_sum_iter(memory: &BitSlice<Msb0, u8>) -> (usize, u64) {
-    let pt = packet_type(memory);
-    let ver = version(memory);
-    if pt == LITERAL {
-        let (offset, val) = read_literal(memory);
-        println!("Found literal: ver={} val={}", ver, val);
-        return (offset, ver);
-    } else {
-        let mut version_sum = ver;
-        match length(memory)  {
-            LengthType::Bits(sz, length_offset) => {
-                println!("Found bits operator len={}, ver {} ", sz, ver);
-                let bits_mem = &memory[6 + length_offset..];        
-                let mut var_offset = 0;
-
-                while var_offset < (sz as usize) {
-                    let (o2, vsum) = version_sum_iter(&bits_mem[var_offset..]);
-                    var_offset += o2;
-                    version_sum += vsum; 
-                }
-                return (6 + length_offset + var_offset, version_sum);
-             
-            }
-            LengthType::Packets(count, length_offset) => {
-                println!("Found packets operator count={}, ver {} ", count, ver);
-                let packets_mem = &memory[6 + length_offset..];
-                let mut packets_offset = 0;
-                for _i in 0..count {
-                    let (o2, vsum) = version_sum_iter(&packets_mem[packets_offset..]);
-                    packets_offset += o2;
-                    version_sum += vsum;
-                }
-                return (6 + length_offset + packets_offset, version_sum);
-            }
-        }
-    }
-}
-
-/*
-
-const SUM : u64 = 0;
-const PRODUCT : u64 = 1;
-const MIN : u64 = 2;
-const MAX : u64 = 3;
-const LITERAL : u64 = 4;
-const GREATER : u64 = 5;
-const LESS : u64 = 6;
-const EQ : u64 = 7;
- */
 
 fn execute(oper: u64, vals: Vec<u64>) -> u64 {
     match oper {
@@ -128,43 +68,64 @@ fn execute(oper: u64, vals: Vec<u64>) -> u64 {
         PRODUCT => vals.iter().product(),
         MIN => *vals.iter().min().unwrap(),
         MAX => *vals.iter().max().unwrap(),
-        GREATER => if vals[0] > vals[1] { 1 } else { 0 }
-        LESS => if vals[0] < vals[1] { 1 } else { 0 }
-        EQ => if vals[0] == vals[1] { 1 } else { 0 }
-        _ => panic!("Unknown oper")
+        GREATER => {
+            if vals[0] > vals[1] {
+                1
+            } else {
+                0
+            }
+        }
+        LESS => {
+            if vals[0] < vals[1] {
+                1
+            } else {
+                0
+            }
+        }
+        EQ => {
+            if vals[0] == vals[1] {
+                1
+            } else {
+                0
+            }
+        }
+        _ => panic!("Unknown oper"),
     }
 }
 
-
-fn eval_iter(memory: &BitSlice<Msb0, u8>) -> (usize, u64) {
+fn eval_iter(memory: &BitSlice<Msb0, u8>) -> (usize, u64, Vec<u64>) {
     let pt = packet_type(memory);
+    let ver = version(memory);
+    let mut versions = Vec::from([ver]);
     if pt == LITERAL {
         let (offset, val) = read_literal(memory);
-        return (offset, val);
+        return (offset, val, versions);
     } else {
         let mut vals = Vec::new();
-        match length(memory)  {
-            LengthType::Bits(sz, length_offset) => {
-                let bits_mem = &memory[6 + length_offset..];        
+        let (length_offset, length ) =  length(memory);
+        match length {
+            LengthType::Bits(sz) => {
+                let bits_mem = &memory[6 + length_offset..];
                 let mut var_offset = 0;
 
                 while var_offset < (sz as usize) {
-                    let (o2, vsum) = eval_iter(&bits_mem[var_offset..]);
+                    let (o2, vsum, sub_vers) = eval_iter(&bits_mem[var_offset..]);
                     var_offset += o2;
                     vals.push(vsum);
+                    versions.extend(sub_vers);
                 }
-                return (6 + length_offset + var_offset, execute(pt, vals));
-             
+                return (6 + length_offset + var_offset, execute(pt, vals), versions);
             }
-            LengthType::Packets(count, length_offset) => {
+            LengthType::Packets(count) => {
                 let packets_mem = &memory[6 + length_offset..];
                 let mut packets_offset = 0;
                 for _i in 0..count {
-                    let (o2, vsum) = eval_iter(&packets_mem[packets_offset..]);
+                    let (o2, vsum, sub_vers) = eval_iter(&packets_mem[packets_offset..]);
                     packets_offset += o2;
                     vals.push(vsum);
+                    versions.extend(sub_vers);
                 }
-                return (6 + length_offset + packets_offset, execute(pt, vals));
+                return (6 + length_offset + packets_offset, execute(pt, vals), versions);
             }
         }
     }
@@ -172,8 +133,7 @@ fn eval_iter(memory: &BitSlice<Msb0, u8>) -> (usize, u64) {
 
 fn version_sum(input: &str) -> u64 {
     let memory = decode(input);
-    println!("bits: {}", memory);
-    version_sum_iter(&memory).1
+    eval_iter(&memory).2.iter().sum()
 }
 
 fn eval(input: &str) -> u64 {
@@ -188,7 +148,6 @@ fn part1(lines: &Vec<String>) -> u64 {
 fn part2(lines: &Vec<String>) -> u64 {
     eval(&lines[0])
 }
-
 
 fn to_u64(memory: &BitSlice<Msb0, u8>) -> u64 {
     let mut pow = 1;
@@ -205,7 +164,7 @@ fn to_u64(memory: &BitSlice<Msb0, u8>) -> u64 {
 fn main() {
     if let Ok(lines) = read_lines("./input/day16.txt") {
         println!("Part 1 (951): {}", part1(&lines));
-        println!("Part 2: {}", part2(&lines)); //3227 too low, 937610646452 too high
+        println!("Part 2: {}", part2(&lines));
     }
 }
 
@@ -214,10 +173,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn day16() {
+        if let Ok(lines) = read_lines("./input/day16.txt") {
+            assert_eq!(951, part1(&lines));
+            assert_eq!(902198718880, part2(&lines));
+        }
+    }
+
+    #[test]
     fn day16_test_literal() {
         let input = "D2FE28";
         let memory = decode(input);
-        println!("{}", &memory);
         assert_eq!(6, version(&memory));
         assert_eq!(4, packet_type(&memory));
         assert_eq!((21, 2021), read_literal(&memory));
@@ -227,12 +193,12 @@ mod tests {
     fn day16_test_sub_packets_bits() {
         let input = "38006F45291200";
         let memory = decode(input);
-        println!("{}", &memory);
         assert_eq!(1, version(&memory));
         assert_eq!(6, packet_type(&memory));
-        
-        match length(&memory) {
-            LengthType::Bits(bits, offset) => {
+
+        let (offset, length) = length(&memory);
+        match  length{
+            LengthType::Bits(bits) => {
                 assert_eq!(bits, 27);
                 let packet_1 = &memory[(6 + offset)..];
                 assert_eq!(6, version(packet_1));
@@ -245,23 +211,21 @@ mod tests {
                 assert_eq!(4, packet_type(packet_2));
 
                 assert_eq!((16, 20), read_literal(&packet_2));
-
             }
-            _ => panic!("Wrong type")
+            _ => panic!("Wrong type"),
         }
     }
-
 
     #[test]
     fn day16_test_sub_packets_packets() {
         let input = "EE00D40C823060";
         let memory = decode(input);
-        println!("{}", &memory);
         assert_eq!(7, version(&memory));
         assert_eq!(3, packet_type(&memory));
-        
-        match length(&memory) {
-            LengthType::Packets(packets, offset) => {
+
+        let (offset, length) = length(&memory);
+        match length {
+            LengthType::Packets(packets) => {
                 assert_eq!(packets, 3);
                 let packet_1 = &memory[(6 + offset)..];
                 assert_eq!(2, version(packet_1));
@@ -270,7 +234,6 @@ mod tests {
                 assert_eq!((11, 1), read_literal(&packet_1));
 
                 let packet_2 = &packet_1[11..];
-                println!("packet 2: {}", packet_2);
                 assert_eq!(4, version(packet_2));
                 assert_eq!(4, packet_type(packet_2));
 
@@ -281,12 +244,10 @@ mod tests {
                 assert_eq!(4, packet_type(packet_3));
 
                 assert_eq!((11, 3), read_literal(&packet_3));
-
             }
-            _ => panic!("Wrong type")
+            _ => panic!("Wrong type"),
         }
     }
-
 
     #[test]
     fn day16_version_sum() {
@@ -295,18 +256,6 @@ mod tests {
         assert_eq!(23, version_sum("C0015000016115A2E0802F182340"));
         assert_eq!(31, version_sum("A0016C880162017C3686B18A3D4780"));
     }
-
-
-    #[test]
-    fn day16_text_example_1() {
-        let input = "8A004A801A8002F478";
-        let input_bytes = input.as_bytes();
-        let memory = BitVec::<Msb0, _>::from_slice(input_bytes).unwrap();
-
-        let version = &memory[0..3];
-        println!("{}", version);
-    }
-
 
     #[test]
     fn day16_part2_examples() {
