90571536fdf5711bb61eab2a5a7f5157f09bd4e1
Ben Gibbs <benjgibbs@gmail.com>
2021-12-25 09:12:28 +0000

    day25 tidied

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -1,90 +1,90 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use aoc::read_lines;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-enum Herd {
-    East,
-    South,
+struct Ocean {
+    south_herd: HashSet<(usize, usize)>,
+    east_herd: HashSet<(usize, usize)>,
+    max_x: usize, 
+    max_y: usize
 }
 
-fn read_input(lines: &Vec<String>) -> ((usize, usize), HashMap<(usize, usize), Herd>) {
-    let mut result = HashMap::new();
-    let mut max_x = usize::MIN;
-    for y in 0..lines.len() {
-        let line: Vec<char> = lines[y].chars().collect();
-        max_x = max_x.max(line.len());
-        for x in 0..line.len() {
-            match line[x] {
-                '>' => result.insert((x, y), Herd::East),
-                'v' => result.insert((x, y), Herd::South),
-                _ => None,
-            };
+impl Ocean {
+    fn read_input(lines: &Vec<String>) ->Ocean {
+        let mut south_herd = HashSet::new();
+        let mut east_herd = HashSet::new();
+
+        let mut max_x = usize::MIN;
+        for y in 0..lines.len() {
+            let line: Vec<char> = lines[y].chars().collect();
+            max_x = max_x.max(line.len());
+            for x in 0..line.len() {
+                match line[x] {
+                    '>' => east_herd.insert((x, y)),
+                    'v' => south_herd.insert((x, y)),
+                    _ => false
+                };
+            }
         }
+    
+        Ocean { south_herd: south_herd, east_herd: east_herd, max_x: max_x, max_y: lines.len() }
     }
 
-    return ((max_x, lines.len()), result);
-}
+    fn move_east(&mut self) -> bool {
+        let mut result = HashSet::<(usize, usize)>::new();
+        let mut has_moved = false;
 
-fn move_east(ocean: &HashMap<(usize, usize), Herd>, max_x: usize) -> HashMap<(usize, usize), Herd> {
-    let mut result = HashMap::<(usize, usize), Herd>::new();
-    for ((x, y), h) in ocean.iter() {
-        result.insert(
-            match *h {
-                Herd::South => (*x, *y),
-                Herd::East => {
-                    let x2 = (x + 1) % max_x;
-                    if ocean.contains_key(&(x2, *y)) {
-                        (*x, *y)
-                    } else {
-                        (x2, *y)
-                    }
-                }
-            },
-            *h,
-        );
+        for (x, y) in self.east_herd.iter() {
+            let x2 = (x + 1) % self.max_x;
+            if self.contains(&(x2, *y)) {
+                result.insert((*x,*y));
+            } else {
+                has_moved = true;
+                result.insert((x2,*y));
+            }
+        }
+        self.east_herd = result;
+        has_moved
     }
-    result
-}
 
-fn move_south(ocean: &HashMap<(usize, usize), Herd>, max_y: usize) -> HashMap<(usize, usize), Herd> {
-    let mut result = HashMap::<(usize, usize), Herd>::new();
-    for ((x, y), h) in ocean.iter() {
-        result.insert(
-            match *h {
-                Herd::East => (*x, *y),
-                Herd::South => {
-                    let y2 = (y + 1) % max_y;
-                    if ocean.contains_key(&(*x, y2)) {
-                        (*x, *y)
-                    } else {
-                        (*x, y2)
-                    }
-                }
-            },
-            *h,
-        );
+    fn move_south(&mut self) -> bool {
+        let mut result = HashSet::<(usize, usize)>::new();
+        let mut has_moved = false;
+
+        for (x, y) in self.south_herd.iter() {
+            let y2 = (y + 1) % self.max_y;
+            if self.contains(&(*x, y2)) {
+                result.insert((*x,*y));
+            } else {
+                has_moved = true;
+                result.insert((*x,y2));
+            }
+        }
+        self.south_herd = result;
+        has_moved
     }
-    result
+
+    fn run(&mut self) -> i32 {
+        let mut step_count = 0;
+        loop {
+            let moved_east = self.move_east();
+            let moved_south = self.move_south();
+            step_count += 1;
+            if !(moved_east || moved_south) {
+                break;
+            }
+        }
+        return step_count;
+    }
+    fn contains(&self, pos: &(usize, usize)) -> bool {
+        self.east_herd.contains(pos) || self.south_herd.contains(pos)
+    }
 }
 
 fn part1(lines: &Vec<String>) -> i32 {
-    let ((max_x, max_y), mut ocean) = read_input(lines);
-
-    let mut step_count = 0;
-    loop {
-        let start = ocean.clone();
-        let move1 = move_east(&start, max_x);
-        let end = move_south(&move1, max_y);
-        step_count += 1;
-        if start == end {
-            break;
-        }
-        ocean = end;
-    }
-    return step_count;
+    let mut ocean = Ocean::read_input(lines);
+    ocean.run()
 }
-
 
 fn main() {
     if let Ok(lines) = read_lines("./input/day25.txt") {
@@ -104,9 +104,9 @@ mod tests {
     }
 
     #[test]
-    fn day25_example2() {
-        if let Ok(lines) = read_lines("./input/example25.txt") {
-            assert_eq!(0, part2(&lines));
+    fn day25_problem() {
+        if let Ok(lines) = read_lines("./input/day25.txt") {
+            assert_eq!(523, part1(&lines));
         }
     }
 }
